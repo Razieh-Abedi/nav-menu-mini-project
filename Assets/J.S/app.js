0f03fb35040342431cb410ff71d11a115f923968
razinewworld <raziyeabedi@gmail.com>
2022-04-24 17:31:13 +0800

    rectified the add item to the cart

--- Assets/J.S/app.js
+++ Assets/J.S/app.js
@@ -1,12 +1,10 @@
-let product = event.target.parentElement;
-let productImg = product.querySelector(".product-img").src;
-let productName = product.querySelector(".product-name").innerText;
-let productPrice = product.querySelector(".product-price").innerText;
-
 document.querySelectorAll(".add-cart-btn").forEach((element) => {
   element.addEventListener("click", (event) => {
+    // console.log(event);
+    // console.log(event.target);
+    let product = event.target.parentElement.parentElement;
     addPurchaseQuantity();
-    addPurchaseItem();
+    addPurchaseItem(product);
   });
 });
 
@@ -15,9 +13,16 @@ function addPurchaseQuantity() {
   ++purchaseQuantity.innerText;
   // purchaseQuantity.innerText++;
 }
-function addPurchaseItem() {
-  let cartItem = `<ul class="item-list list-unstyled d-flex p-2">
-      <li class="ps-4"><img src="${productImg}" alt="" /></li>
+
+function addPurchaseItem(product) {
+  console.log(product);
+  let productImg = product.querySelector(".product-img").src;
+  let productName = product.querySelector(".product-name").innerText;
+  let productPrice = product.querySelector(".product-price").innerText;
+
+  let cartItem = 
+  `<ul class="item-list list-unstyled d-flex p-2">
+      <li class="ps-4"><img src="${productImg}" alt="" class=""/></li>
       <li class="ps-4"><span>${productName}</span></li>
       <li class="ps-4"><span>${productPrice}</span></li>
       <li class="ps-4"><span>product quantity</span></li>
