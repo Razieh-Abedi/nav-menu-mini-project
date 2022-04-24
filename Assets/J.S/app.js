d96e6e6b1a396261ec42645cd5f1a1f5421616dc
razinewworld <raziyeabedi@gmail.com>
2022-04-24 16:22:06 +0800

    add items to the cart functions

--- Assets/J.S/app.js
+++ Assets/J.S/app.js
@@ -1,19 +1,25 @@
+let product = event.target.parentElement;
+let productImg = product.querySelector(".product-img").src;
+let productName = product.querySelector(".product-name").innerText;
+let productPrice = product.querySelector(".product-price").innerText;
+
 document.querySelectorAll(".add-cart-btn").forEach((element) => {
-  addEventListener("click", () => {
+  element.addEventListener("click", (event) => {
     addPurchaseQuantity();
     addPurchaseItem();
   });
 });
+
 function addPurchaseQuantity() {
   let purchaseQuantity = document.getElementById("purchase-quantity");
   ++purchaseQuantity.innerText;
-  //   purchaseQuantity.innerText++;
+  // purchaseQuantity.innerText++;
 }
 function addPurchaseItem() {
   let cartItem = `<ul class="item-list list-unstyled d-flex p-2">
-      <li class="ps-4"><img src="" alt="" />product image</li>
-      <li class="ps-4"><span>product name</span></li>
-      <li class="ps-4"><span>product price</span></li>
+      <li class="ps-4"><img src="${productImg}" alt="" /></li>
+      <li class="ps-4"><span>${productName}</span></li>
+      <li class="ps-4"><span>${productPrice}</span></li>
       <li class="ps-4"><span>product quantity</span></li>
       <li class="ps-4 delete-btn"><i class="bi bi-trash"></i></li>
     </ul>
