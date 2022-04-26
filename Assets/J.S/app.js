1180b62dbee3177c12614f104a661f573d2c98b8
razinewworld <raziyeabedi@gmail.com>
2022-04-26 11:29:52 +0800

    modified the css and htmls code of the page

--- Assets/J.S/app.js
+++ Assets/J.S/app.js
@@ -7,27 +7,42 @@ document.querySelectorAll(".add-cart-btn").forEach((element) => {
     addPurchaseItem(product);
   });
 });
-
+let purchaseQuantity = document.getElementById("purchase-quantity");
 function addPurchaseQuantity() {
-  let purchaseQuantity = document.getElementById("purchase-quantity");
   ++purchaseQuantity.innerText;
   // purchaseQuantity.innerText++;
 }
+function subtractPurchaseQuantity() {
+  let purchaseQuantityNo = purchaseQuantity.innerText;
+  let itemQuantity = document.querySelector(".item-quantity").innerText;
+  purchaseQuantityNo - itemQuantity;
+}
 
 function addPurchaseItem(product) {
-  console.log(product);
+  // console.log(product);
   let productImg = product.querySelector(".product-img").src;
   let productName = product.querySelector(".product-name").innerText;
   let productPrice = product.querySelector(".product-price").innerText;
 
-  let cartItem = 
-  `<ul class="item-list list-unstyled d-flex p-2">
-      <li class="ps-4"><img src="${productImg}" alt="" class=""/></li>
+  let cartItem = `<ul class="item-list list-unstyled d-flex p-2">
+      <li class="ps-4"><img src="${productImg}" alt="" class="purchaseItemImg"/></li>
       <li class="ps-4"><span>${productName}</span></li>
       <li class="ps-4"><span>${productPrice}</span></li>
-      <li class="ps-4"><span>product quantity</span></li>
-      <li class="ps-4 delete-btn"><i class="bi bi-trash"></i></li>
-    </ul>
-    <a href=""> <button>View shopping cart</button></a>`;
+      <li class="ps-4"><span class="item-quantity>1</span></li>
+      <li class="ps-4 delete-btn"><i class="bi bi-trash text-danger delete-icon"></i></li>
+      </ul>
+    <a href="" class="text-decoration-none"> <button class="view-cart-btn d-block m-auto border border-0 rounded">View shopping cart</button></a>`;
   document.querySelector(".cart-items").innerHTML += cartItem;
+}
+let deleteBtn = document.querySelectorAll(".delete.btn");
+deleteBtn.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let addedProduct = event.target.previousSiblingElements;
+    console.log(addedProduct);
+    deleteItem();
+    subtractPurchaseQuantity();
+  });
+});
+function deleteItem() {
+  addedProduct.remove();
 }
