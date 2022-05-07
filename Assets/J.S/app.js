423f320e776bcbd626bf815b774125f370510bcc
razinewworld <raziyeabedi@gmail.com>
2022-05-07 17:34:54 +0800

    added the total price function

--- Assets/J.S/app.js
+++ Assets/J.S/app.js
@@ -1,10 +1,23 @@
 //////Add items to the cart///////
+
+
+
+
 document.querySelectorAll(".add-cart-btn").forEach((element) => {
   element.addEventListener("click", (event) => {
     let product = event.target.parentElement.parentElement;
+    let pInfo = porductInfo(product);
+    let itemCart=document.querySelector(`#ic${product.id}`)
+
+    if (!itemCart) {
+      addItem(pInfo);
+    } else {
+      ++(itemCart.querySelector(".quantity-I-cart").innerText)
+    }
     addQuantity();
-    addItem(product);
-    // calculateTotalPrice();
+    
+    
+   // calculateTotalPrice(pInfo);
   });
 });
 
@@ -14,56 +27,87 @@ function addQuantity() {
 }
 
 function subtractProductQuantity() {
-  let productQuantity = document.querySelector(".product-quantity");
+  let productQuantity = document.querySelector(".quantity-I-cart");
   totalQuantity.innerText -= productQuantity.innerText;
 }
 
-function porductInfo() {
-  let productImg = product.querySelector(".product-img").src;
-  let productName = product.querySelector(".product-name").innerText;
-  let productPrice = product.querySelector(".product-price").innerText;
-  return {
-    img: productImg,
-    name: productName,
-    price: productPrice,
-    // quantity: productQuantity,
-  };
+function porductInfo(pr) {
+  let productId= pr.id
+  let productImg = pr.querySelector(".product-img").src;
+  let productName = pr.querySelector(".product-name").innerText;
+  let productPrice = pr.querySelector(".product-price").innerText;
+  // return {
+  //   img: productImg,
+  //   name: productName,
+  //   price: productPrice,
+  // };
+
+  return { 
+    productId,
+    productImg,
+    productName,
+    productPrice,
+    productQ:1
+  }
 }
 
-function addItem(product) {
-  let pInfo = porductInfo();
+function addItem(pInfo) {
+  
   document.querySelector(".bottom-cart").style.display = "flex";
   document.querySelector(".empty-cart").style.display = "none";
-  let productQuantity = 1;
-  let cartItem = `<div class="item-list d-flex p-1">
-      <div class="ps-2"><img src="${pInfo.img}" alt="" class="item-img"/></div>
-      <div class="ps-2"><span>${pInfo.name}</span></div>
-      <div class="ps-2"><span>${pInfo.price}</span></div>
-      <div class="ps-2 product-quantity"><span>${productQuantity}</span></div>
+  
+  let cartItem = `<div id="ic${pInfo.productId}" class="item-list d-flex p-1">
+      <div class="ps-2"><img src="${pInfo.productImg}" alt="" class="item-img"/></div>
+      <div class="ps-2">${pInfo.productName}</div>
+      <div class="ps-2 price-I-cart">${pInfo.productPrice}</div>
+      <div class="ps-2 quantity-I-cart">${pInfo.productQ}</div>
       <div class="ps-2 delete-btn"><i class="bi bi-trash text-danger"></i></div>
       </div>`;
   document.querySelector(".cart-items").innerHTML += cartItem;
 
   setDeleteItem();
+  calculateTotalPrice(pInfo);
 }
 /////////Delete selected items from the cart///////
 function setDeleteItem() {
   let deleteBtn = document.querySelectorAll(".delete-btn");
   deleteBtn.forEach((item) => {
     item.addEventListener("click", (event) => {
-      let addedProduct = event.target.parentElement.parentElement;
+      let cartItem = event.target.parentElement.parentElement;
+      let priceItem = cartItem.querySelector(".price-I-cart").innerText;
+      let quantityItem = cartItem.querySelector(".quantity-I-cart").innerText;  
       subtractProductQuantity();
+      calculateTotalPrice2(priceItem,quantityItem);
       if (totalQuantity.innerText <= 0) {
         document.querySelector(".bottom-cart").style.display = "none";
         document.querySelector(".empty-cart").style.display = "block";
       }
-      addedProduct.remove();
+      cartItem.remove();
+      
     });
   });
 }
 ////// Calculate the total price////
-// function calculateTotalPrice() {
-//   let price = pInfo.price * productQuantity;
-//   let totalPrice = document.querySelector(".total-price");
-//   totalPrice.innerText += price;
-// }
+
+let totalPrice = document.querySelector(".total-price");
+function calculateTotalPrice(pInfo) {
+  let price = pInfo.productPrice.slice(1) * pInfo.productQ;
+ totalPrice.innerText =Number(totalPrice.innerText)+ Number(price);
+}
+function calculateTotalPrice2(priceItem,quantityItem) {
+  let price = priceItem.slice(1) * quantityItem;
+  totalPrice.innerText =Number(totalPrice.innerText)-Number(price);
+}
+
+function sum(pr1,pr2) {
+  console.log(arguments);
+  let result = pr1+pr2
+  return result;
+}
+
+let product1=20;
+let product2=30;
+
+let s= sum(product1, product2)
+
+console.log(s);
